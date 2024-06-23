cf23875ae15a820c6960c3149150fe50e533318f
arjunss25 <156572908+arjunss25@users.noreply.github.com>
2024-06-23 20:42:35 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -2,36 +2,24 @@ let box1 = document.querySelector(".box1")
 let box2 = document.querySelector(".box2")
 let box3 = document.querySelector(".box3")
 let img1 = document.querySelector('.img1')
+let img = document.querySelectorAll('.img')
 let img2 = document.querySelector('.img2')
 let img3 = document.querySelector('.img3')
 
-let x;
-let y;
 
 
-
-function showCoords(event) {
-     x = event.clientX;
-     y = event.clientY;
-    let text = "X coords: " + x + ", Y coords: " + y;
-    // console.log(text);
-  }
-
-  const rect = box1.getBoundingClientRect();
-  const offsetX = window.event.clientX - rect.left;
-  const offsetY = window.event.clientY - rect.top;
-
-
-box1.addEventListener("mousemove",function(){
+box1.addEventListener("mousemove",function(e){
     img1.style.display = "block"
-    offsetX = x
-    offsetY = y
-
-    showCoords(event)
+    offsetX = e.clientX
+    offsetY = e.clientY
 })
 
-
-
+function imageAnime(){
+     img.forEach(function(img,index){
+        img.style.left = offsetX + "px"
+        img.style.top = offsetY + "px"
+     })
+}
 
 
 
